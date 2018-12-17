1977a82d2f4ca4c5c3bc842e15121809b8bf06ed
caihongwang <caihongwang@dingtalk.com>
2018-12-17 10:37:49 +0800

    优化 配置并测试 地址接口

--- newMall.center/src/test/com/br/newMall/center/service/impl/WX_AddressServiceImplTest.java
+++ newMall.center/src/test/com/br/newMall/center/service/impl/WX_AddressServiceImplTest.java
@@ -2,6 +2,7 @@ package com.br.newMall.center.service.impl;
 
 import com.alibaba.fastjson.JSONObject;
 import com.br.newMall.api.code.NewMallCode;
+import com.br.newMall.api.dto.BoolDTO;
 import com.br.newMall.api.dto.ResultDTO;
 import com.br.newMall.center.service.WX_DicService;
 import com.br.newMall.dao.WX_AddressDao;
@@ -51,10 +52,67 @@ public class WX_AddressServiceImplTest {
 //        paramMap.put("cityId", "131100");
 //        getRegionList(paramMap);
 
+//        Map<String, Object> paramMap = Maps.newHashMap();
+//        paramMap.put("dicType", "street");
+//        paramMap.put("regionId", "131102");
+//        getStreetList(paramMap);
+
         Map<String, Object> paramMap = Maps.newHashMap();
-        paramMap.put("dicType", "street");
+        paramMap.put("uid", "1");
+        paramMap.put("name", "御景西城贵公子");
+        paramMap.put("phone", "17701359899");
+        paramMap.put("provinceId", "130000");
+        paramMap.put("provinceName", "河北省");
+        paramMap.put("cityId", "131100");
+        paramMap.put("cityName", "衡水市");
         paramMap.put("regionId", "131102");
-        getStreetList(paramMap);
+        paramMap.put("regionName", "桃城区");
+        paramMap.put("streetId", "130131102");
+        paramMap.put("streetName", "温塘镇");
+        paramMap.put("detailAddress", "兆丰家园 1403");
+        addAddress(paramMap);
+    }
+
+    public BoolDTO addAddress(Map<String, Object> paramMap) {
+        logger.info("在【service】中添加地址-addAddress,请求-paramMap = {}", JSONObject.toJSONString(paramMap));
+        Integer addNum = 0;
+        BoolDTO boolDTO = new BoolDTO();
+        String uid = paramMap.get("uid") != null ? paramMap.get("uid").toString() : "";
+        String name = paramMap.get("name") != null ? paramMap.get("name").toString() : "";
+        String phone = paramMap.get("phone") != null ? paramMap.get("phone").toString() : "";
+        String provinceId = paramMap.get("provinceId") != null ? paramMap.get("provinceId").toString() : "";
+        String provinceName = paramMap.get("provinceName") != null ? paramMap.get("provinceName").toString() : "";
+        String cityId = paramMap.get("cityId") != null ? paramMap.get("cityId").toString() : "";
+        String cityName = paramMap.get("cityName") != null ? paramMap.get("cityName").toString() : "";
+        String regionId = paramMap.get("regionId") != null ? paramMap.get("regionId").toString() : "";
+        String regionName = paramMap.get("regionName") != null ? paramMap.get("regionName").toString() : "";
+        String streetId = paramMap.get("streetId") != null ? paramMap.get("streetId").toString() : "";
+        String streetName = paramMap.get("streetName") != null ? paramMap.get("streetName").toString() : "";
+        String detailAddress = paramMap.get("detailAddress") != null ? paramMap.get("detailAddress").toString() : "";
+        paramMap.put("status", "0");
+        if (!"".equals(uid) && !"".equals(name)
+                && !"".equals(phone) && !"".equals(detailAddress)
+                && !"".equals(provinceId) && !"".equals(provinceName)
+                && !"".equals(cityId) && !"".equals(cityName)
+                && !"".equals(regionId) && !"".equals(regionName)
+                && !"".equals(streetId) && !"".equals(streetName)) {
+            addNum = wxAddressDao.addAddress(paramMap);
+            if (addNum != null && addNum > 0) {
+                boolDTO.setSuccess(true);
+                boolDTO.setCode(NewMallCode.SUCCESS.getNo());
+                boolDTO.setMessage(NewMallCode.SUCCESS.getMessage());
+            } else {
+                boolDTO.setSuccess(false);
+                boolDTO.setCode(NewMallCode.NO_DATA_CHANGE.getNo());
+                boolDTO.setMessage(NewMallCode.NO_DATA_CHANGE.getMessage());
+            }
+        } else {
+            boolDTO.setSuccess(false);
+            boolDTO.setCode(NewMallCode.ADDRESS_UID_OR_NAME_OR_PHONE_OR_PROVINCEID_OR_PROVINCENAME_OR_CITYID_OR_CITYNAME_OR_REGIONID_OR_REGIONNAME_OR_STREETID_OR_STREETNAME_OR_DETAILADDRESS_IS_NOT_NULL.getNo());
+            boolDTO.setMessage(NewMallCode.ADDRESS_UID_OR_NAME_OR_PHONE_OR_PROVINCEID_OR_PROVINCENAME_OR_CITYID_OR_CITYNAME_OR_REGIONID_OR_REGIONNAME_OR_STREETID_OR_STREETNAME_OR_DETAILADDRESS_IS_NOT_NULL.getMessage());
+        }
+        logger.info("在【service】中添加地址-addAddress,响应-boolDTO = {}", JSONObject.toJSONString(boolDTO));
+        return boolDTO;
     }
 
     public ResultDTO getStreetList(Map<String, Object> paramMap) {
